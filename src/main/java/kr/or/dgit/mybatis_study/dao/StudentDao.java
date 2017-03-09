3e1abc260276bdc19f1e11e23d897f89fc8f1a76
jwSHIN <kimcio505@naver.com>
2017-03-09 12:31:48 +0900

    12:31 new branch selectByNo

--- src/main/java/kr/or/dgit/mybatis_study/dao/StudentDao.java
+++ src/main/java/kr/or/dgit/mybatis_study/dao/StudentDao.java
@@ -7,4 +7,9 @@ import kr.or.dgit.mybatis_study.dto.Student;
 public interface StudentDao {
 	int insertStudent(Student student);
 	List<Student> selecStudentByAll();
+	
+	//과제 테스트까지 완료 branch이용해서
+	int selectStudentByNo(int studNo);
+/*	int updateStudent(Student student);
+	int deleteStudentByNo(int studNo);*/
 }
